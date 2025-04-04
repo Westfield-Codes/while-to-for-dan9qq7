91f7e763354b9531bc8c4db2f8aa42d5c95eb79a
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-04-04 14:38:23 +0000

    done and working

--- multiply-kata.js
+++ multiply-kata.js
@@ -1,13 +1,35 @@
 /* Multiplication */
-
+var questions = 5;
+var curq = 0
+while (curq < questions){
 // Create 2 variables, a & b, and set to ne random integers between 3 and 9
+var a = Math.floor(Math.random()*(9-3)+1)+1;
+var b = Math.floor(Math.random()*(9-3)+1)+1;
 // Set a variable called product to be the product of a and b
+var product = a*b;
 // Set a variable called equation to display  a * b = ??
+var equation = a+" * "+b+" = "+product
 // Ask the equation of the user and store the user input in a variable called answer
+var answer = prompt("solve "+a+" * "+b+" = ")
 // Say "Correct!" if the answer and product match, "Incorrect!" if not.
-
+if (answer == product) alert("correct")
+    else alert("incorrect")
 /* Multiplication whjile Loop */
-// Embed all this code in a While loop and run it 5 times.
+curq = curq + 1;
+alert("curq="+curq);
+}
 
-/* Multiplication For Loop */
-// Convert the while loop to a for loop. 
+alert("now 4loop");
+
+var questions = 5;
+for (curq = 0;curq < questions; curq++){
+var a = Math.floor(Math.random()*(9-3)+1)+1;
+var b = Math.floor(Math.random()*(9-3)+1)+1;
+var product = a*b;
+var equation = a+" * "+b+" = "+product
+var answer = prompt("solve "+a+" * "+b+" = ")
+if (answer == product) alert("correct")
+    else alert("incorrect")
+
+alert("curq="+curq);
+}
--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-var answer = Math.floor(Math.random()*9)+1;
-alert(answer)
-var forW = confirm("for loop or while loop version? ok =while cancel = for")
-if (forW == true){
-/* While Loop Review: Three Guesses */
-
-// Define a variable answer, set equal to a random number between 1 and 10
-
-// Define a variable guess, set equal to 0
-var guess = 0;
-// Define a variable guessCount, set equal to 0
-var guesscount = 0;
-// Let the user guess WHILE guessCount is less than 4
-while (guesscount < 4){
-// Add one to guessCount
-guesscount = guesscount + 1
-// Set guess equal to the user input after "Guess my number"
-guess = prompt("guess my number")
-// If guess is equal to answer, say "Right!" and break the loop
-if (guess == answer)alert("right!");
-else alert("wrong")
-// Otherwise say "Wrong!"
-// End the loop
+/* Multiplication */
+var questions = 5;
+var curq = 0
+while (curq < questions){
+// Create 2 variables, a & b, and set to ne random integers between 3 and 9
+var a = Math.floor(Math.random()*(9-3)+1)+1;
+var b = Math.floor(Math.random()*(9-3)+1)+1;
+// Set a variable called product to be the product of a and b
+var product = a*b;
+// Set a variable called equation to display  a * b = ??
+var equation = a+" * "+b+" = "+product
+// Ask the equation of the user and store the user input in a variable called answer
+var answer = prompt("solve "+a+" * "+b+" = ")
+// Say "Correct!" if the answer and product match, "Incorrect!" if not.
+if (answer == product) alert("correct")
+    else alert("incorrect")
+/* Multiplication whjile Loop */
+curq = curq + 1;
+alert("curq="+curq);
 }
-// If guessCount is equal to 4, say "Out of guesses. The number was ANSWER"
-if (guesscount == 4)alert("out of guesses! the answer was "+answer)
-/* For Loop Conversion */
 
-// Rewrite this as a for loop. 
-alert("Now4loop")
-}
-else{
+alert("now 4loop");
 
-for (guesscount = 0; guesscount<4 && guess != answer; guesscount++)
-{
-    guess = prompt("guess my number")
-    if (guess != answer) alert("Wrong")
-}
-if (guess == answer) alert("your right in "+guesscount+" guesses")
-    else alert("your out of guesses, the answer is "+answer)
+var questions = 5;
+for (curq = 0;curq < questions; curq++){
+var a = Math.floor(Math.random()*(9-3)+1)+1;
+var b = Math.floor(Math.random()*(9-3)+1)+1;
+var product = a*b;
+var equation = a+" * "+b+" = "+product
+var answer = prompt("solve "+a+" * "+b+" = ")
+if (answer == product) alert("correct")
+    else alert("incorrect")
+
+alert("curq="+curq);
 }
